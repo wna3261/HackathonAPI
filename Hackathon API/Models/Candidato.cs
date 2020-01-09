e118f246ec833d187477fc7dc6b0024e042ede86
Weslley Nascimento <weslleynassjn@gmail.com>
2020-01-09 12:15:01 -0300

    Voltando mensagem de erro dos campos invalidos

--- Hackathon API/Models/Candidato.cs
+++ Hackathon API/Models/Candidato.cs
@@ -7,10 +7,13 @@ namespace Hackathon_API.Models
     public class Candidato
     {
         public int Id { get; set; }
-        [StringLength(60, MinimumLength = 3,ErrorMessage = "O campo nome deve ter no minimo 3 caracteres")]
-        [Required(ErrorMessage = "O campo nome é obrigatório")]
+        [StringLength(60, MinimumLength = 3, ErrorMessage = "O campo Nome deve ter no minimo 3 caracteres.")]
+        [Required(ErrorMessage = "O campo Nome é obrigatório.")]
         public string Nome { get; set; }
+        [Required(ErrorMessage = "O campo Cidade é obrigatório.")]
         public string Cidade { get; set; }
+
+        [Required(ErrorMessage = "O campo Nota é obrigatório.")]
         public double Nota { get; set; }
         public bool Situacao { get; set; }
     }
--- Hackathon-SPA/src/app/candidato-create/candidato-create.component.ts
+++ Hackathon-SPA/src/app/candidato-create/candidato-create.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { CandidatoService } from '../_services/candidato.service';
 import { Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
+import { isNullOrUndefined } from 'util';
 
 @Component({
   selector: 'app-candidato-create',
@@ -9,7 +10,7 @@ import { ToastrService } from 'ngx-toastr';
   styleUrls: ['./candidato-create.component.css']
 })
 export class CandidatoCreateComponent implements OnInit {
-  candidato: any = { nome: '', cidade: ''};
+  candidato: any = { nome: '', cidade: '', nota: 0 };
 
   constructor(
     private candidatoService: CandidatoService,
@@ -20,14 +21,25 @@ export class CandidatoCreateComponent implements OnInit {
   }
 
   cadastrarCandidato() {
-    this.candidatoService.cadastrarCandidato(this.candidato).subscribe(data => {
-      this.toastr.success('Cadastrado com Sucesso', 'Sucesso!');
-      this.router.navigate(['home']);
-    }, error => {
-      error.error.errors.Nome.forEach(element => {
-        this.toastr.error(element)
-      })
-    });
+    if (!isNullOrUndefined(this.candidato.nota)) {
+      this.candidatoService.cadastrarCandidato(this.candidato).subscribe(data => {
+        this.toastr.success('Cadastrado com Sucesso', 'Sucesso!');
+        this.router.navigate(['home']);
+      }, error => {
+        error.error.errors ?
+          Object.values(error.error.errors).forEach(fieldErrors => {
+            let errors = fieldErrors as Array<string>;
+            errors.forEach(error => {
+              this.toastr.error(error)
+            });
+          }) :
+          this.toastr.error(error.error);
+      });
+    }
+    else {
+      this.toastr.error("O campo Nota é obrigatório.");
+    }
+
     // this.router.navigate(['home']);
   }
 
